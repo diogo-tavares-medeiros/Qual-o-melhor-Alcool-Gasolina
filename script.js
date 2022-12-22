a07d10b738cf1be23ff9c6b8a4b72c60af13a9e8
Diogo Medeiros <diogotavares00@yahoo.com.br>
2022-12-22 13:36:45 -0300

    abastencedo alcool ou gasolina

---
+++ script.js
@@ -0,0 +1,16 @@
+var etanol, gasolina
+
+function calcular() {
+  etanol = parseFloat(frmFlex.txtEtanol.value.replace(',', '.'))
+  gasolina = parseFloat(frmFlex.txtGasolina.value.replace(',', '.'))
+
+  if (etanol < 0.7 * gasolina) {
+    document.getElementById('status').src = 'image/etanol.png'
+  } else {
+    document.getElementById('status').src = 'image/gasolina.png'
+  }
+}
+
+function resetar() {
+  document.getElementById('status').src = 'image/neutro.png'
+}
